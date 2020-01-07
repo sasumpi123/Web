190c23a1726c153093cc4f31d132e1320b25cf70
임성훈 <sasumpi123@naver.com>
2020-01-07 17:14:40 +0900

    다중삽입

--- jsp01_myboard_sunghun/src/com/test/madeContent.java
+++ jsp01_myboard_sunghun/src/com/test/madeContent.java
@@ -1,16 +1,21 @@
 package com.test;
 
-//import com.dto.MyDto;
-//import com.dao.MyDao;
+import com.dto.MyDto;
+import com.dao.MyDao;
 
 public class madeContent {
 
 	public static void main(String[] args) {
-		/*
-		 * MyDto dto = new MyDto(); MyDao dao = new MyDao();
-		 * 
-		 * dto.setMyname("123"); dto.setMytitle("123"); dto.setMycontent("123"); for(int
-		 * i=0; i<350; i++) { dao.insert(dto); }
-		 */
+		MyDto dto = new MyDto();
+		MyDao dao = new MyDao();
+
+		dto.setMyname("123");
+		dto.setMytitle("123");
+		dto.setMycontent("123");
+		for (int i = 0; i < 350; i++) {
+			dao.insert(dto);
+		}
+
 	}
+
 }
